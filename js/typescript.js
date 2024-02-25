83e916699c240eb293d118ed138478409914d20b
ismael <moreiraismael14@gmail.com>
2024-02-24 21:57:11 -0300

    update brabo mas o php ainda ta com erro

---
+++ js/typescript.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".descricao", {
+    strings: [
+        "Dev Web.",
+        "Dev Front-end.",
+        "Dev Back-end.",
+        ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
